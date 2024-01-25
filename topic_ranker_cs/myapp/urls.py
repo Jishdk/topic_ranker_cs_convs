4e940996ef2e1ac24ec7f7f1beaa7d3ea20ec0fc
Ryuuk <66210297+Jishdk@users.noreply.github.com>
2024-01-25 16:21:53 +0100

    Delete topic_ranker_cs directory

--- topic_ranker_cs/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('results/<int:topic_id>/<str:topic_name>/', views.results, name='results'),
-]
